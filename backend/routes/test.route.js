44dfee50b5637df2ef0913159d1f445e3ebe87db
Ra-h-ul <rahulpandita25@gmail.com>
2024-08-08 19:10:09 +0530

    Socket io added

--- backend/routes/test.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import  {loggedin}  from '../controller/test.controller.js';
-import {verifyToken} from '../middleware/verifyToken.js';
-const router = express.Router();
-
-router.get("/logged-in" ,verifyToken,  loggedin);
-
-export default router;

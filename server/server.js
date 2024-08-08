872de95aeb65b5d5939d56975dd01b22e3ea632f
Ra-h-ul <rahulpandita25@gmail.com>
2024-08-08 23:01:29 +0530

    build command added

--- server/server.js
+++ server/server.js
@@ -8,6 +8,15 @@ import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
+// resolving absolute path
+import path from 'path';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+
+
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -20,6 +29,13 @@ app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
+
+
+// to use the client app
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+// Render client
+app.get('*',(req,res)=> res.sendFile(path.join(__dirname,'/client/dist/index.html')))
 
 app.listen(8800, () => {
   console.log("Server is running!");

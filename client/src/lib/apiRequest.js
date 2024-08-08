ffec7116398d4f266cef467abee1621a420d1c80
Rahul pandita <61160462+Ra-h-ul@users.noreply.github.com>
2024-08-08 19:41:26 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://real-estate-mern-app-woad.vercel.app/",
+  baseURL: "http://localhost:5173",
   withCredentials: true,
 });
 
